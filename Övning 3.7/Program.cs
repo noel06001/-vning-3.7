f42f03082d6fec3e193c00eb0c3571ab4f7b9f9e
noell <noell@DESKTOP-NLQ10I7>
2023-10-07 18:41:04 +0200

    Add project files.

---
+++ Övning 3.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Övning_3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder  = int.Parse(Console.ReadLine());
+
+            if ((ålder >= 10 && ålder <=18) || ålder >= 65)
+            {
+                Console.WriteLine("Du får rabatt på kaffet.");
+            }
+            else
+            {
+                Console.WriteLine("Tyvärr så får du ingen rabatt på ditt kaffe.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
